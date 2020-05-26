e74256a01c0b7086c680a4f74f6e73535dddc0e6
Alfredo Gonzalez <56003346+Dakoth@users.noreply.github.com>
2020-05-25 20:47:54 -0700

    Added timer stuff to main.c

--- source/main.c
+++ source/main.c
@@ -8,10 +8,67 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-#include "timer.h"
+//#include "timer.h"
+
+//TIMER STUFF
+///////////////////////////////////////////////////////////////////////
+volatile unsigned char TimerFlag = 0; //stuff added 
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	//AVR timer/counter controller register ....
+	TCCR1B = 0x0B;
+
+	//AVR output compare register ....
+	OCR1A = 125;
+
+	//AVR timer interrupt mask register
+	TIMSK1 = 0x02;
+
+	//initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	//TimerIRS called every _avr ... milliseconds
+	//
+	
+	//enable global interrupts
+	SREG |= 0x80; //0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; //timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+//TimerISR() calls this 
+ISR(TIMER1_COMPA_vect) {
+	//cpu calls when TCNT1 == OCR1
+	_avr_timer_cntcurr--;	//counts down to 0
+
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();	//calls ISR that user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+
+	}
+}
+
+//Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
 
 /* SM state declarations --- fill in as needed */
 typedef enum output_states { OInit, Ooutput } output_states;
